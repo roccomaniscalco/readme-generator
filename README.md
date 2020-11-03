894680d17bf482a42b308586e74382771dd41d14
Rocco <roccomaniscalco2001@gmail.com>
2020-11-03 03:02:56 -0500

    created README template

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# readme-generator
+![shield](https://img.shields.io/badge/license-MIT-blue)
+
+# Title
+
+## Description
+
+## Usage
+
+## Installation
+
+## Contributing
+
+## Tests
+
+## Questions
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 
+// array of questions for user
 const questions = [
   {
     name: "title",
@@ -32,11 +33,22 @@ const questions = [
   },
 ];
 
-inquirer
+// function to initialize program
+function init() {
+  inquirer
   .prompt(questions)
   .then((answers) => {
-    console.log("Success!");
+    writeToFile("README.md",answers);
   })
   .catch((error) => {
     if (error.isTtyError) "error";
   });
+}
+
+// function to write README file
+function writeToFile(fileName, data) {
+  console.log(data);
+}
+
+// function call to initialize program
+init();
